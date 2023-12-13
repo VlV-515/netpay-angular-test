dcadc74497cc062775476a7bc734ce0b261927be
Ignacio Tirado <92279938+VlV-515@users.noreply.github.com>
2023-12-12 19:55:14 -0600

    Cascaron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'netpay-angular-test';
+  inpCantidad = 0;
+  resultadoTokenizar = {};
+  constructor() {}
+
+  public btnTokenizar(): void {
+    console.log(this.inpCantidad);
+  }
 }
